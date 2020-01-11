252e4055185469aec3c865982aa81c69027663df
Hadeel Elmadhoon <helmadh@uwo.ca>
2020-01-11 18:03:19 -0500

    pull trips

--- shehacks4/lib/experiences.dart
+++ shehacks4/lib/experiences.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ExperiencesPage extends StatelessWidget {
+// final db = Firestore.instance;
+
+// void getData() {
+//   db
+//       .collection("experiences")
+//       .getDocuments()
+//       .then((QuerySnapshot snapshot) {
+//     snapshot.documents.forEach((f) => print('${f.data}}'));
+//   });
+// }
+
+// class ExperiencesPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context){
+//     return Container(
+//       child: Text('Experiences'),
+//     );
+//   }
+// }
+
+class ExperiencesPage extends StatefulWidget {
+  @override
+  ExperiencesPageState createState() => ExperiencesPageState();
+}
+
+class ExperiencesPageState extends State<ExperiencesPage> {
+  
+  Future getExperiences() async{
+    var db = Firestore.instance;
+    QuerySnapshot qn = await db.collection("experience").getDocuments();
+    return qn.documents;
+  }
+  
   @override
   Widget build(BuildContext context){
     return Container(
-      child: Text('Experiences'),
+      child: FutureBuilder(
+        future: getExperiences(),
+        builder: (_, snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Center(
+            child: Text("Loading..."),
+            );
+        } else {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (_, index){
+                return ListTile(
+                  title: Text(snapshot.data[index].data["name"]),
+                );
+              });
+        }
+      }),
     );
   }
+
 }
+
