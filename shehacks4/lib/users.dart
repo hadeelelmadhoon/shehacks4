503994ae5f02d5fac68cf58d5d707a344c326fb4
Nadeen E <nelshaw@uwo.ca>
2020-01-12 00:11:10 -0500

    Display users based on experience

--- shehacks4/lib/users.dart
+++ shehacks4/lib/users.dart
@@ -11,37 +11,62 @@ class UsersPage extends StatefulWidget {
 class UsersPageState extends State<UsersPage> {
   final String name;
   UsersPageState(this.name);
+  final db = Firestore.instance;
+  List<DocumentSnapshot> users; 
+
   Future getUsers() async{
     var db = Firestore.instance;
     Query query = db.collection("experience").where('name', isEqualTo: name);
+    // print(query.toString());
+    return query.getDocuments();
+  }
+
+  Future _getData() {
+    db.collection("experience").where('name', isEqualTo: name)
+    .getDocuments()
+    .then((QuerySnapshot snapshot) {
+      users = snapshot.documents;
+      // print(users[0].data["users"]);
+      return snapshot.documents;
+    });
   }
 
   @override
   Widget build(BuildContext context){
-    return Container(
-      child: FutureBuilder(
-        future: getUsers(),
-        builder: (_, snapshot){
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return Center(
-            child: Text("Loading..."),
-            );
-        } else {
-            return ListView.builder(
-              // itemCount: snapshot.data.length,
-              // for(int i = 0; i<snapshot.data.length; i++)
-
-
-              
-              // itemBuilder: (_, index){    
-                itemBuilder: (_, index2){
-                  return ListTile(
-                  title: Text(snapshot.data[index2].data["users"]),
-                );
-                
-              });
-        }
-      }),
+    // return Container(
+    //   child: FutureBuilder(
+    //     future: getData(),
+    //     builder: (_, snapshot){
+    //       if(snapshot.connectionState == ConnectionState.waiting){
+    //         return Center(
+    //           child: Text("Loading..."),
+    //           );
+    //       } else {
+    //         getData();
+    //         return Container(
+    //           child: Text(users[0].data["users"].toString())
+    //         );
+    //     }
+    //   }),
+    // );
+    return Scaffold(
+      body: FutureBuilder<List<dynamic>>(
+        future: _getData(),
+        builder: (context, snapshot) {
+          print(users[0].data);
+          if (users[0] == null) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return ListView(
+            children: users.map((data) {
+              print(data);
+              return ListTile(
+                title: Text(data["users"].toString()),
+              );
+            }).toList(),
+          );
+        },
+      ),
     );
   }
 
